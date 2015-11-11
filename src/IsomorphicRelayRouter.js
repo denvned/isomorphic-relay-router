5158012146dd9aff87defa4fd63010f5dcf859d7
Denis Nedelyaev <denvned@gmail.com>
2015-11-11 19:11:07 +0300

    Update isomorphic-relay version

--- src/IsomorphicRelayRouter.js
+++ src/IsomorphicRelayRouter.js
@@ -1,3 +1,5 @@
+import 'isomorphic-relay'; // load before "react-relay" to prevent "self is not defined"
+
 export {default as createIsomorphicElement} from './createIsomorphicElement'
 export {default as loadAndStoreData} from './loadAndStoreData'
 export {default as storePreloadedData} from './storePreloadedData'
