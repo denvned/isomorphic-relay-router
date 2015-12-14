035494dde47147548fa67465425b3a88ab014985
Denis Nedelyaev <denvned@gmail.com>
2015-12-14 08:04:23 +0300

    Remove an unused file

--- examples/todo/src/rootContainerProps.js
+++
@@ -1,9 +0,0 @@
-import StarWarsApp from './components/StarWarsApp';
-import StarWarsAppHomeRoute from './routes/StarWarsAppHomeRoute';
-
-export default {
-    Component: StarWarsApp,
-    route: new StarWarsAppHomeRoute({
-        factionNames: ['empire', 'rebels']
-    }),
-};
