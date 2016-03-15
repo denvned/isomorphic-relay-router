2fc1850bc54f3b408f8e34fa244f5e9ccd65ee7f
Denis Nedelyaev <denvned@gmail.com>
2016-03-15 11:17:43 +0300

    Minor cleanup

--- src/IsomorphicRouter.js
+++ src/IsomorphicRouter.js
@@ -1,5 +1,3 @@
-// load before "react-relay" to prevent "self is not defined"
-import 'isomorphic-relay';
 import IsomorphicRelayRouter from './IsomorphicRelayRouter';
 import IsomorphicRelayRouterContext from './IsomorphicRelayRouterContext';
 import prepareData from './prepareData';
