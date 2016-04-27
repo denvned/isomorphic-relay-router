de2a22ffc06473d4f9d4ba11afee9a62b34e8940
Denis Nedelyaev <denvned@gmail.com>
2016-04-27 13:52:04 +0300

    Add workaround for server side route matching (fixes #26)

--- src/prepareData.js
+++ src/prepareData.js
@@ -3,6 +3,9 @@ import RouteAggregator from 'react-router-relay/lib/RouteAggregator';
 import render from './render';
 
 export default function prepareData(renderProps, networkLayer) {
+  // HACK: workaround for https://github.com/denvned/isomorphic-relay-router/issues/26
+  renderProps.router.replace(renderProps.location);
+
   const routeAggregator = new RouteAggregator();
   routeAggregator.updateRoute(renderProps);
 
