035180eba43b6eead2a1c7c434206cf2db372ff4
KCraw <kristopher.craw@gmail.com>
2016-05-19 09:21:06 -0500

    fix: modify render to allow passing middleware or props

--- src/render.js
+++ src/render.js
@@ -3,8 +3,12 @@ import { applyRouterMiddleware } from 'react-router';
 import useRelay from 'react-router-relay';
 import IsomorphicRelayRouterContext from './IsomorphicRelayRouterContext';
 
-export default function(otherMiddleware = []) {
-  return applyRouterMiddleware(...otherMiddleware, {
+export default function(propsOrMiddleware = []) {
+  if (propsOrMiddleware.render) {
+    return propsOrMiddleware.render(propsOrMiddleware);
+  }
+
+  return applyRouterMiddleware(...propsOrMiddleware, {
     renderRouterContext: (child, props) => (
       <IsomorphicRelayRouterContext {...props}>
         {child}
